d8255fccfc17dc485936d3ffc824cc0b9c539fdf
deivy-spaulonci <deivy.spaulonci@hotmail.com>
2020-11-12 22:44:47 -0300

    iniciando

---
+++ src/main/java/com/br/testedisys/TestedisysApplication.java
@@ -0,0 +1,13 @@
+package com.br.testedisys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedisysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedisysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/testedisys/TestedisysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.testedisys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedisysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
